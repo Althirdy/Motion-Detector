4360fed67bf5f60671785adf15f099abb41586b6
Althirdy <althirdysanger@gmail.com>
2024-12-18 07:32:46 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,25 @@ import { Gauge, gaugeClasses } from "@mui/x-charts/Gauge";
 import toast from "react-hot-toast";
 
 function App() {
-  const [sensorValue, setSensorValue] = useState(null);
+  const [motion, setMotion] = useState(null);
+  const [motionCount,setMotionCount] = useState(0)
+
   useEffect(() => {
-    const sensorRef = ref(database, "Sensor/ldr_data");
+    const sensorRef = ref(database, "Sensor/data");
 
     const unsubscribe = onValue(sensorRef, (snapshot) => {
       const value = snapshot.val();
-      setSensorValue(value);
+      setMotion(value);
     });
 
+    
     return () => unsubscribe();
+  
   }, []);
 
   // Customize the toast inside useEffect
   useEffect(() => {
-    if (sensorValue > 1000) {
+    if (motion == 1) {
       toast.custom((t) => (
         <div
           style={{
@@ -35,45 +39,20 @@ function App() {
             fontWeight: "bold",
           }}
         >
-          <strong>⚠️ WARNING!</strong>
-          <span style={{ marginLeft: "8px" }}>Gas leak detected!</span>
+          <strong>⚠️</strong>
+          <span style={{ marginLeft: "8px" }}>Motion Detected</span>
         </div>
       ));
+      setMotionCount(prev => prev + 1 );
     }
-  }, [sensorValue]);
+  }, [motion]);
   return (
     <div className="max-w-screen-lg mx-auto flex items-center min-h-screen justify-center">
       <div className="flex flex-col items-center p-4">
         <h1 className="text-4xl font-bold text-gray-900 text-center">
-          Gas Leak Detector Simple IoT
+          Motion Detector
         </h1>
-        <p
-          className={`text-center text-2xl font-medium ${
-            sensorValue > 1000 ? "text-red-600" : " text-gray-800"
-          }`}
-        >
-          {sensorValue !== null ? `Gas level: ${sensorValue}` : "Loading..."}
-        </p>
-        <Stack
-          direction={{ xs: "column", md: "row" }}
-          spacing={{ xs: 1, md: 3 }}
-        >
-          <Gauge
-            width={200}
-            height={200}
-            value={sensorValue}
-            valueMin={0}
-            valueMax={1000}
-            sx={(theme) => ({
-              [`& .${gaugeClasses.valueText}`]: {
-                fontSize: 15,
-              },
-              [`& .${gaugeClasses.valueText} text`]: {
-                fill: "#000000", // <-- change text color
-              },
-            })}
-          />
-        </Stack>
+        <p>Motion Count: {motionCount}</p>
       </div>
     </div>
   );
